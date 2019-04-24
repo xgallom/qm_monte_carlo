7d34bb6fde9867e7ff0e4768b0d658f686038961
Milan Gallo <gallo.milan.jr@gmail.com>
2019-04-24 02:18:26 +0200

    Woohoo
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- src/config.h
+++ src/config.h
@@ -8,24 +8,21 @@
 namespace Config
 {
 	constexpr size_t
-			WalkerCount = 150,
-			Steps = 6000,
-			ThermStep = 1,
-			Therm = 1000,
+			WalkerCount = 300,
+			Steps = 60000,
+			ThermStep = 1000,
+			Therm = 10000,
 
 			TrialsAlpha = 40,
-			TrialsBeta = 1,
 
 			PointsSize = (Steps - Therm) / ThermStep;
 
 	constexpr double
-			dR = 10,
+			dR = 10.,
 			Alpha1 = 0.1,
 			dAlpha = 0.05,
-			Beta1 = 1.9,
-			dBeta = 0.05,
 
-			Dist = 2.;
+			Dist = 0.5;
 }
 
 #endif //QM_MONTE_CARLO_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -12,48 +12,19 @@ namespace
 		Random::init();
 	}
 
-	double prob(const Vector<Vector3D> &r, double a, double b)
+	double prob(double r, double a)
 	{
-		const auto
-				r1 = abs(r[0]),
-				r2 = abs(r[1]);
-
-		//return sqr(exp(-a * r1 - b * r2) + exp(-b * r1 - a * r2));
-		return exp(-2. * a * sqr(r[0][0]));
+		return exp(-2. * a * sqr(r));
 	}
 
-	double energy(const Vector<Vector3D> &r, double a, double b)
+	double energy(double r, double a)
 	{
-		const auto xs = sqr(r[0][0]);
-
-		return a + xs * (0.5 - 2. * sqr(a));
-
-		const auto
-				ra = abs(r[1] - r[0]),
-				r1 = abs(r[0]),
-				r2 = abs(r[1]),
-
-				as = sqr(a),
-				bs = sqr(b);
-
-		const auto V = 2. / r1 + 2. / r2 - 1. / ra;
-		const auto wave = exp(-a * r1 - b * r2) + exp(-b * r1 - a * r2);
-
-		return -0.5 * (as + bs) + (
-										  (a * exp(-a * r1 - b * r2) + b * exp(-b * r1 - a * r2)) / r1 +
-										  (b * exp(-a * r1 - b * r2) + a * exp(-b * r1 - a * r2)) / r2
-								  ) / wave - V;
+		return a + sqr(r) * (0.5 - 2. * sqr(a));
 	}
 
-	Vector<Vector3D> step(const Vector<Vector3D> &r)
+	double step(double r)
 	{
-		Vector<Vector3D> result = {};
-		result.reserve(r.size());
-
-		for(const auto &rn : r)
-			result.push_back(rn + Random::vectorOffset() * Config::dR);
-
-		return result;
+		return std::min(10., std::max(-10., r + Random::get() * Config::dR));
 	}
 }
 
@@ -61,50 +32,41 @@ int main()
 {
 	init();
 
-	Matrix<Config::TrialsBeta, Config::TrialsAlpha> A, B, E = {};
+	VectorD A = {}, E = {};
+	A.reserve(Config::TrialsAlpha);
+	E.reserve(Config::TrialsAlpha);
 
-	for(size_t n = 0; n < A.size(); ++n) {
-		const auto c = A(n);
-
-		B[n] = Config::Beta1 + Config::dBeta * c.row;
-		A[n] = Config::Alpha1 + Config::dAlpha * c.col;
-	}
+	for(size_t n = 0; n < Config::TrialsAlpha; ++n)
+		A.push_back(Config::Alpha1 + Config::dAlpha * n);
 
 	std::cout << A << "\n\n";
-	std::cout << B << "\n\n";
 
-	double minEnergy = 0., minA, minB;
+	double minEnergy = 0., minA = 0.;
 
-	for(size_t n = 0; n < A.size(); ++n) {
-		const auto
-			a = A[n],
-			b = B[n];
-
+	for(const auto &a : A) {
 		auto allEnergy = 0.;
 
-		std::cout << "" << n << "\n";
+		std::cout << "a: " << a << "\n";
+
 		for(size_t w = 0; w < Config::WalkerCount; ++w) {
-			Vector<Vector3D> r = {Random::vector() / 2., Random::vector() / 2.};
+			auto r = Random::dist();
 
-			r[0][0] = std::max(10., std::min(-10., r[0][0]));
-
-			Vector<double> points = {};
+			VectorD points = {};
 			points.reserve(Config::PointsSize);
 
 			for(size_t s = 0; s < Config::Steps; ++s) {
 				const auto rNew = step(r);
 
-				const auto p = prob(rNew, a, b) / prob(r, a, b);
+				const auto p = prob(rNew, a) / prob(r, a);
 
 				if(p >= Random::norm())
 					r = rNew;
 
 				if(s > Config::Therm && (s - Config::Therm) % Config::ThermStep == 0)
-					points.push_back(energy(r, a, b));
+					points.push_back(r);
 			}
 
-			const auto walkerEnergy = accumulate(points) / points.size();
-			allEnergy += walkerEnergy;
+			allEnergy += accumulate(transform(points, [&a](const double &r) { return energy(r, a); })) / points.size();
 		}
 
 		const auto avgEnergy = allEnergy / Config::WalkerCount;
@@ -113,18 +75,17 @@ int main()
 		if(avgEnergy < minEnergy) {
 			minEnergy = avgEnergy;
 			minA = a;
-			minB = b;
 		}
 
-		E[n] = avgEnergy;
+		E.push_back(avgEnergy);
 	}
 
 	std::ofstream f("out.txt");
 	for(int n = 0; n < A.size(); ++n)
-		f << A[n] << " " << B[n] << " " << E[n] << "\n";
+		f << A[n] << " " << E[n] << "\n";
 	f.close();
 
-	std::cout << "Minimums: " << minEnergy << " " << minA << " " << minB << "\n";
+	std::cout << "Minimums: " << minEnergy << " " << minA << "\n";
 
 	return 0;
 }
