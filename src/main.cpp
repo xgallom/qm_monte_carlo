9807c5295dbe88f1da4faf7af35973d81edbfeeb
Milan Gallo <gallo.milan.jr@gmail.com>
2020-06-07 22:15:26 +0200

    main.cpp refactoring
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,72 +13,63 @@
 #include <sstream>
 #include <random>
 
+constexpr double
+	SpaceDistMax = 2. * BohrRadius,
+	DeltaDistMax = 0.05,
+	
+	SeparationMin = 0.3 * BohrRadius,
+	SeparationMax = 6.3 * BohrRadius,
+	SeparationStep = 0.05 * BohrRadius;
+
+constexpr size_t
+	Tries = 1u << 10u,
+	Steps = 1u << 13u,
+	Skips = 1u << 8u,
+	SkipsMask = Skips - 1u;
+
 int main()
 {
 	std::random_device randomDevice;
 	std::mt19937 generator(randomDevice());
 
 	std::uniform_real_distribution<double>
-			normalDist(0, 1.0),
-			spaceDist(-2 * BohrRadius, 2 * BohrRadius),
-			deltaDist(-0.05, 0.05);
+			normalDist(0.0, 1.0),
+			spaceDist(-SpaceDistMax, SpaceDistMax),
+			deltaDist(-DeltaDistMax, DeltaDistMax);
 
-	std::fstream
-			file("out.txt", std::ios::out);//,
-//			debug("debug.txt", std::ios::out);
+	std::fstream file("out.txt", std::ios::out);
 
 	if(!file.is_open())// || !debug.is_open())
 		return 1;
 
 
 	size_t n = 0;
-	for(double S = 0.3 * BohrRadius; S < 6.3 * BohrRadius; S += 0.05 * BohrRadius, ++n) {
-
-		Parameters parameters = generateParameters(S);
-
+	for(double S = SeparationMin; S <= SeparationMax; S += SeparationStep) {
 		std::stringstream ss;
 		ss << "debug_" << n << ".txt";
 		std::fstream debug(ss.str(), std::ios::out);
 
-//		debug << "Starting simulation\n";
-//		debug << "parameters: " << parameters << "\n\n";
+		Parameters parameters = generateParameters(S);
 
 		double averageEnergy = 0.;
-
-		for(size_t tries = 0; tries < 1u << 10u; ++tries) {
+		for(size_t tries = 0; tries < Tries; ++tries) {
 			Context context = generateContext(generator, spaceDist, parameters, S);
 
 			double totalEnergy = 0.;
 			size_t count = 0;
 
-			double minimum = 0.;
-			Context minimumContext = {};
-
-
-			for(size_t step = 0; step < (1u << 13u); ++step) {
+			for(size_t step = 0; step < Steps; ++step) {
 				Context newContext = generateContext(context, generator, deltaDist, parameters, S);
 
-				if(/*newContext.waveContext.waveSquared < 1.0 &&*/
-						newContext.waveContext.waveSquared / context.waveContext.waveSquared >= normalDist(generator)
-						)
+				if(newContext.waveContext.waveSquared / context.waveContext.waveSquared >= normalDist(generator))
 					context = newContext;
 
-				if(!(step & ((1u << 8u) - 1))) {
+				if(!(step & SkipsMask)) {
 					double energy = localEnergy(context, parameters);
-
-					if(energy < minimum) {
-						minimum = energy;
-						minimumContext = context;
-					}
 
 					totalEnergy += energy;
 
-//					debug << "[" << step << "]\n";
-//					debug << "context: " << context << "\n";
-//					debug << "energy: " << energy << " J = " << (energy / ElectronCharge) << " eV\n";
-//					debug << "totalEnergy: " << totalEnergy << " J = " << (totalEnergy / ElectronCharge) << " eV\n\n";
-
-				debug
+					debug
 						<< (context.electronConfiguration.e1.x / BohrRadius) << " "
 						<< (context.electronConfiguration.e1.y / BohrRadius) << " "
 						<< (context.electronConfiguration.e1.z / BohrRadius) << " "
@@ -101,19 +92,21 @@ int main()
 				}
 			}
 
-//			std::cout << "Separation: " << (S / BohrRadius) << "\n";
-//			std::cout << "Minimum energy: " << (minimum / ElectronCharge) << "\n";
-//		std::cout << "Minimum context: " << minimumContext << "\n\n";
-
 			const double result = 2. * Q2 / S + totalEnergy / count;
-//			std::cout << "\nResult: " << result << " J = " << (result / ElectronCharge) << " eV\n\n";
-
 			averageEnergy += result;
 		}
 
-		std::cout << "Separation: " << (S / BohrRadius) << "\nResult: " << (averageEnergy / double(1u << 10u) / ElectronCharge) << "\n\n";
-		file << (S / BohrRadius) << " " << (averageEnergy / double(1u << 10u) / ElectronCharge) << "\n";
+		std::cout 
+			<< "Separation: " << (S / BohrRadius) << "\n"
+			<< "Result: " << (averageEnergy / Tries / ElectronCharge) << "\n\n";
+
+		file
+			<< (S / BohrRadius) << " " 
+			<< (averageEnergy / Tries / ElectronCharge) << "\n";
+
 		file.flush();
+
+		++n;
 	}
 
 	file.close();
